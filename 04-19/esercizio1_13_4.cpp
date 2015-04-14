203a15e010bb518c0938dc6c69c0d2decd1c0ad7
fedsib <fedsib@users.noreply.github.com>
2015-04-14 20:07:52 +0200

    modificato es1_13_4 filè approved

--- 04-19/esercizio1_13_4.cpp
+++ 04-19/esercizio1_13_4.cpp
@@ -5,28 +5,19 @@ using namespace std;
 
 bool match(int *x, int *P, int dimP, int lim3){
 	bool ok=true;
-	int cont =0;
 	for(int i=0; i<dimP && ok; i++)
-		if(x[i*lim3] != -1){
 			if(x[i*lim3]!=P[i])
 				ok=false;
-			else cont++;
-		}
-		else ok=false;
-	
-	if(cont == dimP)
-		ok=true;
-	else ok = false;
-	
 	return ok;
 }
 
-int contaMatch(int *x, int lim1, int lim2, int lim3, int *P, int dimP){
-	int n_match=0, lim=lim1*lim2;
-	for(int i=0; i<lim-dimP+1; i++)
+int contaMatch(int *x, int lung, int lim3, int *P, int dimP, ofstream& OUT){
+	int n_match=0;
+	for(int i=0; i<lung-dimP+1; i++)
 		if(match(x, P, dimP, lim3)){
 			n_match++;
 			x=x+dimP*lim3;
+			i++;
 		}
 		else
 			x=x+lim3;
@@ -36,7 +27,7 @@ int contaMatch(int *x, int lim1, int lim2, int lim3, int *P, int dimP){
 int main(){
 	ifstream IN("input");
 	ofstream OUT("output");
-	int quanti =0, garbage;
+	
 	if(IN && OUT){
 	int X[400], *x = X, P[50], lim1, lim2, lim3, n_el, dimP, match=0, vfmax=0;
 
@@ -45,18 +36,8 @@ int main(){
 	for(int i=0; i<400; i++)
 		X[i] = -1;
 	
-	for(int i=0; i<lim1; i++)
-		for(int j=0; j<lim2; j++)
-			for(int k=0; k<lim3 && i*lim2*lim3+j*lim3+k<n_el; k++)
-				IN>>X[i*lim2*lim3+j*lim3+k];
-	
-	quanti = n_el - (lim1*lim2*lim3);
-	if(quanti)
-		for(int i =0; i < quanti; i++)
-			IN >> garbage;
-
-	//for(int i=0; i<n_el; i++)
-		//IN >> x[i];
+	for(int i=0; i<n_el; i++)
+		IN >> x[i];
 
 	IN >> dimP;
 
@@ -69,10 +50,26 @@ int main(){
 	
 	IN.close();
 	
+	int nsp = n_el/(lim2*lim3);
+	int	nrp, rim;
+	if(nsp >=lim1){
+		nrp=lim1*lim2;
+		rim =0;
+	}
+	else  {
+		nrp=n_el/lim3;
+		rim = n_el%lim3;
+	}
+	
+	int lung = nrp;
+	
 	for(int i=0; i < vfmax; i++){
-		match = contaMatch(x+i,lim1,lim2,lim3,P,dimP);
+		if(i<rim)
+			lung++;
+		match = contaMatch(x+i,lung,lim3,P,dimP, OUT);
 		OUT << "V-fetta " << i << " = " << match << " match" << endl;
 		match=0;
+		lung = nrp;
 	}
 
 	}
