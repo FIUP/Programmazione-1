8078b19a8c8a7edc0fc3681f38a8e33d5915cabd
sara <sara.righetto123@gmail.com>
2018-02-25 20:07:03 +0100

    Aggiunti esercizi2017_2018

---
+++ Esercizi assegnati/2017-2018/esercizio3/esercizio3.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    // delimitatore iniziale output del programma (stringa "start")\
+	// ATTENZIONE! Non cancellare l'istruzione sottostante
+	cout << "start" << endl;
+
+	// inserire qui il codice del programma
+
+    int a,b,c;
+	cin>>a>>b>>c;
+
+	if (a>0 && b>0 && c>0) {
+		if (a<c && b<c ){ 
+			if(a+b>c){cout<<"Si"<<endl;}
+			else cout<<"No"<<endl;}
+		if (a<b && c<b ){ 
+			if(a+c>b){cout<<"Si"<<endl;}
+			else cout<<"No"<<endl;}
+		if (c<a && b<a ){ 
+			if(c+b>a){cout<<"Si"<<endl;}
+			else cout<<"No"<<endl;}
+	}
+	else{cout<<"Input sbagliato"<<endl;}
+
+
+
+	// delimitatore finale output del programma (stringa "end")
+	// ATTENZIONE! Non cancellare l'istruzione sottostante
+	cout << "end" << endl;
+}
