72e694339e16e3759c4f8ffd0446254eb607eac8
Timoty <timoty.granziero@gmail.com>
2018-09-08 13:44:52 +0200

    Rename files (#13); improve coding style

--- Esercizi assegnati/2017-2018/esercizio05/esercizio5.cpp
+++ Esercizi assegnati/2017-2018/esercizio05/esercizio5.cpp
@@ -2,40 +2,39 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
 	// delimitatore iniziale output del programma (stringa "start")
 	// ATTENZIONE! Non cancellare l'istruzione sottostante
 	cout << "start" << endl;
 
 	// inserire qui il codice del programma
 
-    char c;		
-	int count=0;	//contatore
+    char c;
+	int count = 0;	//contatore
+
 	do {
 		cin >> c;
-		
-		if (c=='(')
-			count ++;
-			
-		else if (c==')') {
-		    count --;
-		}
-		
-	} while(c!='0' && count>=0);
-	
-	if(count ==0)
+
+		if (c == '(')
+			count++;
+		else if (c == ')')
+		    count--;
+	} while (c != '0' && count >= 0);
+
+	if (count == 0)
 		cout << "la sequenza e' bilanciata";
-		else{
-			cout << "la sequenza non e' bilanciata";
-			if (count<0) 
-		    	{cout << ": ci sono parentesi chiuse senza match";}
-		    	else {cout<< ": ci sono parentesi aperte senza match";}
-		}
-	
-	
- cout <<endl;
+	else {
+		cout << "la sequenza non e' bilanciata";
+		if (count < 0)
+			cout << ": ci sono parentesi chiuse senza match";
+		else
+			cout << ": ci sono parentesi aperte senza match";
+	}
+	cout << endl;
+
 	// delimitatore finale output del programma (stringa "end")
 	// ATTENZIONE! Non cancellare l'istruzione sottostante
 	cout << "end" << endl;
+
+	return 0;
 }
